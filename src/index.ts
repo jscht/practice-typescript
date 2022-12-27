076239a0657bfcd859c47b92bbdb15505108066b
jscht <80654820+jscht@users.noreply.github.com>
2022-12-27 18:13:32 +0900

    block class merge

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("ready")
+import crypto from "crypto"
+
+/**
+ * @argument hash - Hash value of block
+ * @argument prevHash - Previous block
+ * @argument height - Total number of blocks
+ * @argument data - Protected data
+ */
+interface BlockShape {
+    hash: string
+    prevHash: string
+    height: number
+    data: string
+}
+
+class Block implements BlockShape {
+    public hash: string
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.hashFn(prevHash, height, data)
+    }
+    /**
+     * Calculate Hash
+     * @param {string} prevHash
+     * @param {number} height
+     * @param {string} data
+     * @returns {string} hash value
+     */
+    static hashFn(prevHash: string, height: number, data: string) {
+        const toHash = `${prevHash}${height}${data}`
+        return crypto.createHash("sha256").update(toHash).digest("hex")
+        
+    } // static => 인스턴스가 생성되지 않아도 호출 가능한 함수
+}
